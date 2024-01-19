729c2d48a714a86d1ab18307a05bd4ad098d4503
Khairnar <sahil.khairnar@accenture.com>
2024-01-19 16:53:51 +0900

    User Registration added

--- src/main/java/com/practice/ticketapp/TicketappApplication.java
+++ src/main/java/com/practice/ticketapp/TicketappApplication.java
@@ -2,8 +2,10 @@ package com.practice.ticketapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.practice.ticketapp")
 public class TicketappApplication {
 
 	public static void main(String[] args) {
